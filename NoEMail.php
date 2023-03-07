c3923927e9e868bf1fa0a504d289f6c33b78d4f1
iandreme <121745334+iandreme@users.noreply.github.com>
2023-03-07 07:51:40 +0000

    $EMail

--- NoEMail.php
+++ NoEMail.php
@@ -2,11 +2,11 @@
 <body>
 <?php 
 $EMail =
-$_GET["EMail"];
+$_GET["EMail"],
 $_GET["NoEMail"]; 
 
-echo $_POST['EMail'];
-echo $_POST['NoEMail'];
+echo $_POST['EMail'],
+echo $_POST['NoEMail'],
 echo $EMail;
 ?>
 </body>
